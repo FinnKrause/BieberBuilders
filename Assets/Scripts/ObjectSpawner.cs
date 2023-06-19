739c62666904229fff696d6d26acb18ccc4814d8
00JAN010 <jsimon.gieb@gmail.com>
2023-06-19 09:31:19 +0200

    randomiser verbessert nicht ganz fertig

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -11,6 +11,15 @@ public class ObjectSpawner : MonoBehaviour
     public float spawnInterval;
     public float spawnXRange = 6;
 
+    private float zahl;
+    private int obi = 1;
+    private float bombe = 1;
+    private float blatt = 1;
+    private float holz = 1;
+    public float mehrBombe;
+    public float mehrBlatt;
+    public float mehrHolz;
+
     void Start()
     {
         _spawnY = Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height+20, 0f)).y;
@@ -26,9 +35,34 @@ public class ObjectSpawner : MonoBehaviour
         {
             float spawnX = UnityEngine.Random.Range(-spawnXRange, spawnXRange);
             Vector2 spawnPosition = new Vector2(spawnX, _spawnY);
-            GameObject rbdObj = Prefabs[UnityEngine.Random.Range(0, Prefabs.Length)];
+            GameObject rbdObj = Prefabs[Objectwaehler()];
             Instantiate(rbdObj, spawnPosition, Quaternion.identity);
             yield return new WaitForSeconds(spawnInterval);
         }
     }
+
+    private int Objectwaehler(){
+        zahl = UnityEngine.Random.Range(0,bombe+blatt+holz);
+        if(zahl<=bombe){
+            obi = 0;
+            blatt = blatt*mehrBlatt;
+            holz = holz*mehrHolz;
+            bombe = 1;
+            return obi;
+        }else if(zahl<=bombe+blatt){
+            obi = 1;
+            bombe = bombe*mehrBombe;
+            holz = holz*mehrHolz;
+            blatt = 1;
+            return obi;
+        }else if(zahl<=bombe+blatt+holz){
+            obi = 2;
+            bombe = bombe*mehrBombe;
+            blatt = blatt*mehrBlatt;
+            holz = 1;
+            return obi;
+    }
+return 1;
+
+}
 }

92d430aa84911ff87e7b3d03bb99d79249891fb7
LucasLandrith <lucas.a.landrith@gmail.com>
2023-06-23 00:06:25 +0200

    Dash Improvements and English

--- Assets/Scripts/BieberDash.cs
+++ Assets/Scripts/BieberDash.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 public class BieberDash : MonoBehaviour
 {
     private UIBar _energyBar;
-    private bool dashMachen;
+    private bool performDash;
 
-    public float energiekostenDash;
-    public float stärkeDash;
-    public float randRechts;
-    void Start()
+    public float dashEnergyCost;
+    public float dashStrength;
+    public float rightBoundary;
+
+    private void Start()
     {
         _energyBar = GameObject.Find("EnergyBar").GetComponent<UIBar>();
     }
 
-    
-    void Update()
+    private void Update()
     {
-        float _movementDirectionX = Input.GetAxis("Horizontal") * stärkeDash;
-        dashMachen = Input.GetKey("space");
+        float movementDirectionX = Input.GetAxis("Horizontal") * dashStrength;
+        performDash = Input.GetKey("space");
 
-        if (dashMachen == true) {   //will der spieler dashen
-                if (transform.position.x + _movementDirectionX > randRechts) { _movementDirectionX = randRechts - transform.position.x; }
-                if (transform.position.x + _movementDirectionX < -randRechts) { _movementDirectionX = -randRechts - transform.position.x; } // nicht über rand
-        
-            transform.Translate(_movementDirectionX,0,0);
-            _energyBar.subtract(energiekostenDash);
+        if (performDash)
+        {
+            movementDirectionX = Mathf.Clamp(movementDirectionX, -rightBoundary - transform.position.x, rightBoundary - transform.position.x);
+            transform.Translate(movementDirectionX, 0, 0);
+            _energyBar.subtract(dashEnergyCost);
         }
     }
 }

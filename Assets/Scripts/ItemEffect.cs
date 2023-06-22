09f77afebd6b6a068803852e9c6e62c8e48745db
LucasLandrith <lucas.a.landrith@gmail.com>
2023-06-23 01:47:52 +0200

    ItemEffects Simple

--- Assets/Scripts/ItemEffect.cs
+++ Assets/Scripts/ItemEffect.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WelchesItemBinich : MonoBehaviour
+public class ItemEffect// : MonoBehaviour
 {
+    public string EffectName { get; private set; }
+
+    public ItemEffect(string effectName)
+    {
+        EffectName = effectName;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemGenerator : MonoBehaviour
-{   //basiert auf Object spawner, eigenes Script um Items unabhängiger steuern zu können und überschneidungen zu vermeiden
-    private float _spawnY;
+{ 
+    private float spawnY;
 
     public GameObject prefab;
     public float initialSpawnInterval = 1f;
-    public float spawnXRange = 6;
+    public float spawnXRange = 6f;
     public float spawnInterval;
 
     public int nextItem;
-    public int wievieleItems;
+    public int numberOfItems;
+
+    private Dictionary<int, ItemEffect> itemEffects;
 
     void Start()
     {
-        _spawnY = Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height + 20f, 0f)).y;
+        spawnY = Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height + 20f, 0f)).y;
         spawnInterval = initialSpawnInterval;
+
+        // Initialize item effects
+        itemEffects = new Dictionary<int, ItemEffect>();
+        itemEffects.Add(0, new ItemEffect("Effect 1"));
+        itemEffects.Add(1, new ItemEffect("Effect 2"));
+        itemEffects.Add(2, new ItemEffect("Effect 3"));
+        // Add more items and their respective effects as needed
+
+        SetNextItemAuto();
         StartCoroutine(SpawnObjects());
     }
 
@@ -28,18 +39,34 @@ public class ItemGenerator : MonoBehaviour
             yield return new WaitForSeconds(spawnInterval);
 
             float spawnX = Random.Range(-spawnXRange, spawnXRange);
-            Vector2 spawnPosition = new Vector2(spawnX, _spawnY);
+            Vector2 spawnPosition = new Vector2(spawnX, spawnY);
 
-            setNextItemAuto();
-            Instantiate(prefab, spawnPosition, Quaternion.identity); //Prefab an ort, unrotiert
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+
+            SetNextItemAuto();
         }
     }
 
-    private void setNextItemAuto()
+    private void SetNextItemAuto()
     {
-        nextItem = Random.Range(0, wievieleItems);
+        nextItem = Random.Range(0, numberOfItems);
+        ApplyItemEffect(nextItem);
+    }
+
+    private void ApplyItemEffect(int itemNumber)
+    {
+        if (itemEffects.ContainsKey(itemNumber))
+        {
+            ItemEffect itemEffect = itemEffects[itemNumber];
+            // Apply the effect specific to the item number
+            Debug.Log("Applying item effect: " + itemEffect.EffectName);
+        }
+        else
+        {
+            Debug.LogWarning("No item effect defined for item number: " + itemNumber);
+        }
     }
 }
 
-    //Weiterer Plan für die Items ist, dass sie in ihrer Startmethode die nächste Zahl bekommen ud speichern
-    //Der Itemeffeckt soll von der Zahl abhängen
+/*every item effect is represented by the ItemEffect class that has a property for the effect name
+add methods for every new effect*/
